0c7095050dabb636147f2d55192de94846653eba
kvlet <vletsiskostas@gmail.com>
2019-12-04 00:56:42 +0200

    layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-9 d-none d-md-block p-0">
-                    <div class="bg-ekthesis"></div>
+                    <div class="bg-formekthesis"></div>
                 </div>
                 <div class="col-md-3 d-flex justify-content-center align-items-center flex-row login-column">
                     <div class="login-form">
