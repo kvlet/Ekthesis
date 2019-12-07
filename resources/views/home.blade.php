c27076051e4b93876d0971ce88f2debe04f770e0
kvlet <vletsiskostas@gmail.com>
2019-12-07 15:56:53 +0200

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,70 +38,124 @@
                         <li class="nav-item"><a class="nav-link py-0" href="/jobs"><span>Εργασίες</span></a></li>
                     </ul>
                 </div>
-    </li>
+        </li>
         <li class="nav-item">
-                <a class="nav-link collapsed text-truncate" href="#submenu1" data-toggle="collapse" data-target="#submenu1"><i class="fa fa-table"></i> <span class="d-none d-sm-inline">Διαχείριση Οικονομικών</span></a>
-                    <div class="collapse" id="submenu1" aria-expanded="false">
+            <a class="nav-link collapsed text-truncate" href="#submenu4" data-toggle="collapse" data-target="#submenu4"><i class="fa fa-table"></i> <span class="d-none d-sm-inline">Διαχείριση Οικονομικών</span></a>
+                <div class="collapse" id="submenu4" aria-expanded="false">
+                    <ul class="flex-column pl-2 nav">
+                        <li class="nav-item"><a class="nav-link py-0" href="/timolcomp"><span>Τιμολόγια Εταιρείας</span></a></li>
+                        <li class="nav-item"><a class="nav-link py-0" href="/eisprcomp"><span>Είσπραξη Τιμολογίων Εταιρειας</span></a></li>
+                        <li class="nav-item"><a class="nav-link py-0" href="/coreisprcomp"><span>Διόρθωση Εισπραξης Εταιρείας</span></a></li>
+                        <li class="nav-item"><a class="nav-link py-0" href="/timolpartner"><span>Τιμολόγια Συναργάτη</span></a></li>
+                        <li class="nav-item"><a class="nav-link py-0" href="/carexpen"><span>Εξόδα Οχήματος</span></a></li>
+                        <li class="nav-item"><a class="nav-link py-0" href="/carincome"><span>Έσοδα Οχήματος</span></a></li>
+                        <li class="nav-item"><a class="nav-link py-0" href="/expenmanage"><span>Διαχείριση Εξόδων</span></a></li>
+                    </ul>
+                </div>
+        </li>
+        <li class="nav-item">
+                <a class="nav-link text-truncate" href="/managekeimena"><i class="fa fa-home"></i> <span class="d-none d-sm-inline">Διαχείριση Κειμένων</span></a>
+        </li>
+        <li class="nav-item">
+                <a class="nav-link collapsed text-truncate" href="#submenu5" data-toggle="collapse" data-target="#submenu5"><i class="fa fa-table"></i> <span class="d-none d-sm-inline">Εκτυπώσεις</span></a>
+                    <div class="collapse" id="submenu5" aria-expanded="false">
                         <ul class="flex-column pl-2 nav">
-                            <li class="nav-item"><a class="nav-link py-0" href="#"><span>Orders</span></a></li>
                              <li class="nav-item">
-                                <a class="nav-link collapsed py-1" href="#submenu1sub1" data-toggle="collapse" data-target="#submenu1sub1"><span>Customers</span></a>
+                                <a class="nav-link collapsed py-1" href="#submenu1sub1" data-toggle="collapse" data-target="#submenu1sub1"><span>Ελεγκτικές/Στατιστικές</span></a>
                                     <div class="collapse" id="submenu1sub1" aria-expanded="false">
                                         <ul class="flex-column nav pl-4">
                                             <li class="nav-item">
                                                  <a class="nav-link p-1" href="#">
-                                                    <i class="fa fa-fw fa-clock-o"></i> Daily </a>
+                                                    <i class="fa fa-fw fa-clock-o"></i> Προς διαικπαιρέωση </a>
                                             </li>
                                             <li class="nav-item">
                                                 <a class="nav-link p-1" href="#">
-                                                    <i class="fa fa-fw fa-dashboard"></i> Dashboard </a>
+                                                    <i class="fa fa-fw fa-dashboard"></i> Εκπρόθεσμες </a>
                                             </li>
                                             <li class="nav-item">
                                                 <a class="nav-link p-1" href="#">
-                                                     <i class="fa fa-fw fa-bar-chart"></i> Charts </a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link p-1" href="#">
-                                                    <i class="fa fa-fw fa-compass"></i> Areas </a>
+                                                     <i class="fa fa-fw fa-bar-chart"></i> Στατιστικές </a>
                                             </li>
                                         </ul>
                                     </div>
                                 </li>
                         </ul>
-                    </div>
-        </li>
-        <li class="nav-item">
-                <a class="nav-link text-truncate" href="/pragmatognomosines"><i class="fa fa-home"></i> <span class="d-none d-sm-inline">Διαχείριση Κειμένων</span></a>
-        </li>
-        <li class="nav-item">
-                <a class="nav-link collapsed text-truncate" href="#submenu1" data-toggle="collapse" data-target="#submenu1"><i class="fa fa-table"></i> <span class="d-none d-sm-inline">Εκτυπώσεις</span></a>
-                    <div class="collapse" id="submenu1" aria-expanded="false">
                         <ul class="flex-column pl-2 nav">
-                            <li class="nav-item"><a class="nav-link py-0" href="#"><span>Orders</span></a></li>
-                             <li class="nav-item">
-                                <a class="nav-link collapsed py-1" href="#submenu1sub1" data-toggle="collapse" data-target="#submenu1sub1"><span>Customers</span></a>
-                                    <div class="collapse" id="submenu1sub1" aria-expanded="false">
-                                        <ul class="flex-column nav pl-4">
-                                            <li class="nav-item">
-                                                 <a class="nav-link p-1" href="#">
-                                                    <i class="fa fa-fw fa-clock-o"></i> Daily </a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link p-1" href="#">
-                                                    <i class="fa fa-fw fa-dashboard"></i> Dashboard </a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link p-1" href="#">
-                                                     <i class="fa fa-fw fa-bar-chart"></i> Charts </a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a class="nav-link p-1" href="#">
-                                                    <i class="fa fa-fw fa-compass"></i> Areas </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </li>
-                        </ul>
+                                <li class="nav-item">
+                                   <a class="nav-link collapsed py-1" href="#submenu1sub2" data-toggle="collapse" data-target="#submenu1sub2"><span>Τιμολόγησης</span></a>
+                                       <div class="collapse" id="submenu1sub2" aria-expanded="false">
+                                           <ul class="flex-column nav pl-4">
+                                               <li class="nav-item">
+                                                    <a class="nav-link p-1" href="#">
+                                                       <i class="fa fa-fw fa-clock-o"></i> Προς Τιμολόγηση </a>
+                                               </li>
+                                               <li class="nav-item">
+                                                   <a class="nav-link p-1" href="#">
+                                                       <i class="fa fa-fw fa-dashboard"></i> Τιμολογημένες </a>
+                                               </li>
+                                           </ul>
+                                       </div>
+                                   </li>
+                           </ul>
+                           <ul class="flex-column pl-2 nav">
+                                <li class="nav-item">
+                                   <a class="nav-link collapsed py-1" href="#submenu1sub3" data-toggle="collapse" data-target="#submenu1sub3"><span>Οικονομικές Γραφείου</span></a>
+                                       <div class="collapse" id="submenu1sub3" aria-expanded="false">
+                                           <ul class="flex-column nav pl-4">
+                                               <li class="nav-item">
+                                                    <a class="nav-link p-1" href="#">
+                                                       <i class="fa fa-fw fa-clock-o"></i> Βεβαιώσεις φόρου </a>
+                                               </li>
+                                               <li class="nav-item">
+                                                   <a class="nav-link p-1" href="#">
+                                                       <i class="fa fa-fw fa-dashboard"></i> Έσοδα </a>
+                                               </li>
+                                               <li class="nav-item">
+                                                   <a class="nav-link p-1" href="#">
+                                                        <i class="fa fa-fw fa-bar-chart"></i> Έξοδα </a>
+                                               </li>
+                                           </ul>
+                                       </div>
+                                   </li>
+                           </ul>
+                           <ul class="flex-column pl-2 nav">
+                                <li class="nav-item">
+                                   <a class="nav-link collapsed py-1" href="#submenu1sub4" data-toggle="collapse" data-target="#submenu1sub4"><span>Οικονομικές Εταιρειών</span></a>
+                                       <div class="collapse" id="submenu1sub4" aria-expanded="false">
+                                           <ul class="flex-column nav pl-4">
+                                               <li class="nav-item">
+                                                    <a class="nav-link p-1" href="#">
+                                                       <i class="fa fa-fw fa-clock-o"></i> Εκκαθάριση Τιμολογίων </a>
+                                               </li>
+                                               <li class="nav-item">
+                                                   <a class="nav-link p-1" href="#">
+                                                       <i class="fa fa-fw fa-dashboard"></i> Έλεγχος Τιμολογίων </a>
+                                               </li>
+                                               <li class="nav-item">
+                                                   <a class="nav-link p-1" href="#">
+                                                        <i class="fa fa-fw fa-bar-chart"></i> Υπόλοιπα </a>
+                                               </li>
+                                           </ul>
+                                       </div>
+                                   </li>
+                           </ul>
+                           <ul class="flex-column pl-2 nav">
+                                <li class="nav-item">
+                                   <a class="nav-link collapsed py-1" href="#submenu1sub5" data-toggle="collapse" data-target="#submenu1sub5"><span>Οικονομικές Συνεργατών</span></a>
+                                       <div class="collapse" id="submenu1sub5" aria-expanded="false">
+                                           <ul class="flex-column nav pl-4">
+                                               <li class="nav-item">
+                                                    <a class="nav-link p-1" href="#">
+                                                       <i class="fa fa-fw fa-clock-o"></i> Εκκαθάριση Συναργάτη </a>
+                                               </li>
+                                               <li class="nav-item">
+                                                   <a class="nav-link p-1" href="#">
+                                                       <i class="fa fa-fw fa-dashboard"></i> Έλεγχος Τιμολογίων </a>
+                                               </li>
+                                           </ul>
+                                       </div>
+                                   </li>
+                           </ul>
                     </div>
         </li>
     </ul>
