cc290360b498f3a993428930147e1ccb1a066bf2
kvlet <vletsiskostas@gmail.com>
2019-12-11 14:51:35 +0200

    Update entolietaireias.blade.php

--- resources/views/reports/checkstats/prosdiekpereosi/entolietaireias.blade.php
+++ resources/views/reports/checkstats/prosdiekpereosi/entolietaireias.blade.php
@@ -1,2 +1,12 @@
-<?php
-<a href="{{ asset('reports/Entoli_etaireias.pdf') }}">Open the pdf!</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="{{ asset('reports/Entoli_etaireias.pdf') }}">Open the pdf!</a>
+</body>
+</html>
