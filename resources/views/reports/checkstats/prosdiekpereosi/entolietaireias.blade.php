92e68c253efe45e8a33d63df78b36d7e2305f48a
kvlet <vletsiskostas@gmail.com>
2019-12-12 10:11:37 +0200

    Update entolietaireias.blade.php

--- resources/views/reports/checkstats/prosdiekpereosi/entolietaireias.blade.php
+++ resources/views/reports/checkstats/prosdiekpereosi/entolietaireias.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Εντολή Εταιρείας</title>
 </head>
 <body>
-    <a href="{{ asset('reports/Entoli_etaireias.pdf') }}">Open the pdf!</a>
+    <a href="{{ asset('/reports/Entoli_etaireias.pdf') }}">Open the pdf!</a>
 </body>
 </html>
