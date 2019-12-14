7ef5ba1188c1f0981d8d7db25a0f44461931c7dc
kvlet <vletsiskostas@gmail.com>
2019-12-14 20:06:56 +0200

    v

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -72,7 +72,7 @@
                                                         <i class="fa fa-fw fa-clock-o"></i>Εντολή Εταιρείας</a>
                                                     </li>
                                                     <li class="nav-item">
-                                                        <a class="nav-link p-1" href="/reports/checkstats/prosdiekpereosi/anathesisesinergati">
+                                                        <a class="nav-link p-1" href={{ url('/reports/checkstats/prosdiekpereosi/anathesisesinergati.blade.php') }}>
                                                         <i class="fa fa-fw fa-dashboard"></i> Ανάθεση σε Συνεργάτη </a>
                                                     </li>
                                                     <li class="nav-item">
