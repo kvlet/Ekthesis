d446e5e970e16abe3e286ec5d1dd930f2d7ecd6d
kvlet <vletsiskostas@gmail.com>
2019-12-17 11:45:51 +0200

    v

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,21 +50,7 @@
                                      <p>Εδώ τον user</p>
                                 </div>
                                 <div class="col-md-4">
-                                   <!-- <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                       {{-- Auth::user()->name --}} <span class="caret"></span>
-                                    </a>-->
-
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            {{ __('Logout') }}
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            @csrf
-                                        </form>
-                                    </div>
+                                    <p>Εδώ το logout</p>
                                 </div>
                          </div>
                         <div class="row">
