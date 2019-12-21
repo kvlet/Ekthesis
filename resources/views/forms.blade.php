eee20a8e200a693948c2f50c40a23f5fac947a24
kvlet <vletsiskostas@gmail.com>
2019-12-21 22:46:37 +0200

    v

---
+++ resources/views/forms.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+@include('layouts._partials.header')
+
+@section('onoffmenu')
+    <ul class="nav flex-column flex-nowrap overflow-hidden">
+        <li class="nav-item">
+            <input type="checkbox" onclick="myFunction()" checked data-toggle="toggle">
+        </li>
+        <script type="text/javascript">
+            function myFunction() {
+                var e = document.getElementById("lmenou");
+                if(e.style.display == null || e.style.display == "none") {
+                    e.style.display = "block";
+                } else{
+                    e.style.display = "none";
+                }
+            }
+        </script>
+    </ul>
+@endsection
+@section('lmenu')
+    @include('lmenu')
+@endsection
+
+@section('forms')
+    <div class="bg-form-ekthesis">
+        @yield('form')
+    </div>
+@endsection
+
+@section('rmenu')
+  <p>Εδω τα κουμπια</p>
+@endsection
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,6 @@
 @extends('layouts.app')
 @include('layouts._partials.header')
 
-@section('onoffmenu')
-         <ul class="nav flex-column flex-nowrap overflow-hidden">
-             <li class="nav-item">
-                 <input type="checkbox" onclick="myFunction()" checked data-toggle="toggle">
-             </li>
-             <script type="text/javascript">
-                 function myFunction() {
-                     var e = document.getElementById("lmenou");
-                     if(e.style.display == null || e.style.display == "none") {
-                          e.style.display = "block";
-                     } else{
-                         e.style.display = "none";
-                     }
-                 }
-             </script>
-         </ul>
-@endsection
 @section('lmenu')
     @include('lmenu')
 @endsection
@@ -25,7 +8,3 @@
 @section('forms')
     <div class="bg-wel-ekthesis"></div>
 @endsection
-
-<!--@section('rmenu')-->
-<!--   <p>Εδω τα κουμπια</p>
-@endsection-->
