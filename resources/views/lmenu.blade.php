39a23feacc837d29d7dff3ba11aca650a87dc9b2
kvlet <vletsiskostas@gmail.com>
2019-12-24 16:10:43 +0200

    v

--- resources/views/lmenu.blade.php
+++ resources/views/lmenu.blade.php
@@ -10,7 +10,7 @@
         <div class="collapse" id="submenu1" aria-expanded="false">
             <ul class="flex-column pl-2 nav">
                 <li class="nav-item"><a class="nav-link py-0" href="/pathon"><span>Παθόντες/Υπαίτιοι</span></a></li>
-                <li class="nav-item"><a class="nav-link py-0" href="/usersview"><span>Χρήστες</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="/usersView"><span>Χρήστες</span></a></li>
             </ul>
         </div>
     </li>
@@ -38,18 +38,18 @@
         <a class="nav-link collapsed text-truncate" href="#submenu4" data-toggle="collapse" data-target="#submenu4"><i class="fa fa-table"></i> <span class="d-none d-sm-inline">Οικονομικά</span></a>
         <div class="collapse" id="submenu4" aria-expanded="false">
             <ul class="flex-column pl-2 nav">
-                <li class="nav-item"><a class="nav-link py-0" href="/timolcomp"><span>Τιμολόγια Εταιρείας</span></a></li>
-                <li class="nav-item"><a class="nav-link py-0" href="/eisprcomp"><span>Είσπραξη Τιμολογίων Εταιρειας</span></a></li>
-                <li class="nav-item"><a class="nav-link py-0" href="/coreisprcomp"><span>Διόρθωση Εισπραξης Εταιρείας</span></a></li>
-                <li class="nav-item"><a class="nav-link py-0" href="/timolpartner"><span>Τιμολόγια Συναργάτη</span></a></li>
-                <li class="nav-item"><a class="nav-link py-0" href="/carexpen"><span>Εξόδα Οχήματος</span></a></li>
-                <li class="nav-item"><a class="nav-link py-0" href="/carincome"><span>Έσοδα Οχήματος</span></a></li>
-                <li class="nav-item"><a class="nav-link py-0" href="/expenmanage"><span>Διαχείριση Εξόδων</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="/timolComp"><span>Τιμολόγια Εταιρείας</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="/eisprComp"><span>Είσπραξη Τιμολογίων Εταιρειας</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="/corEisprComp"><span>Διόρθωση Εισπραξης Εταιρείας</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="/timolPartner"><span>Τιμολόγια Συναργάτη</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="/carExpen"><span>Εξόδα Οχήματος</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="/carIncome"><span>Έσοδα Οχήματος</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="/expenManage"><span>Διαχείριση Εξόδων</span></a></li>
             </ul>
         </div>
     </li>
     <li class="nav-item">
-        <a class="nav-link text-truncate" href="/managekeimena"><i class="fa fa-home"></i> <span class="d-none d-sm-inline">Κείμενα</span></a>
+        <a class="nav-link text-truncate" href="/manageKeimena"><i class="fa fa-home"></i> <span class="d-none d-sm-inline">Κείμενα</span></a>
     </li>
     <li class="nav-item">
         <a class="nav-link collapsed text-truncate" href="#submenu5" data-toggle="collapse" data-target="#submenu5"><i class="fa fa-table"></i> <span class="d-none d-sm-inline">Εκτυπώσεις</span></a>
