bbc3ae20b4cff745cf9204fe3d728dbb0af4283f
Miaritis Greg <gregmiaritis@aol.com>
2019-12-29 19:54:10 +0200

    Pragmatognomosini model

---
+++ app/Pragmatognomosini.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pragmatognomosini extends Model
+{
+    protected $table = 'keno';
+
+    protected $fillable = [
+        'grafeio',
+    ];
+}
