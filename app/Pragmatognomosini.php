87fe8a427ebe7d1212ab7a7d4eac9a794ce6850f
kvlet <vletsiskostas@gmail.com>
2019-12-31 01:24:16 +0200

    Update Pragmatognomosini.php

--- app/Pragmatognomosini.php
+++ app/Pragmatognomosini.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Pragmatognomosini extends Model
 {
-    protected $table = 'keno';
+    protected $table = 'db_ekthesis';
 
     protected $fillable = [
         'grafeio',
