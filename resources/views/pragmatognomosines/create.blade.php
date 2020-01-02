b8844bc3b4809e323dd385925ea47fbdec0cfe16
kvlet <vletsiskostas@gmail.com>
2020-01-03 00:34:07 +0200

    try to build the form for create

--- resources/views/pragmatognomosines/create.blade.php
+++ resources/views/pragmatognomosines/create.blade.php
@@ -6,7 +6,7 @@
         <div class="col-2 menu-text-size">
             <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
                 <a class="nav-link active" id="v-pills-genika-tab" data-toggle="pill" href="#v-pills-genika" role="tab" aria-controls="v-pills-genika" aria-selected="true">Γενικά</a>
-                <a class="nav-link" id="v-pills-antikatastasis-tab" data-toggle="pill" href="#v-pills-antikatastasis" role="tab" aria-controls="v-pills-antikatastasis" aria-selected="false">Αντικαταστάσεις</a>
+{{--                <a class="nav-link" id="v-pills-antikatastasis-tab" data-toggle="pill" href="#v-pills-antikatastasis" role="tab" aria-controls="v-pills-antikatastasis" aria-selected="false">Αντικαταστάσεις</a>
                 <a class="nav-link" id="v-pills-epanafores-tab" data-toggle="pill" href="#v-pills-epanafores" role="tab" aria-controls="v-pills-epanafores" aria-selected="false">Επαναφορές</a>
                 <a class="nav-link" id="v-pills-eksagogi-tab" data-toggle="pill" href="#v-pills-eksagogi" role="tab" aria-controls="v-pills-eksagogi" aria-selected="false">Εξαγωγή/Επανατοποθέτηση</a>
                 <a class="nav-link" id="v-pills-bafes-tab" data-toggle="pill" href="#v-pills-bafes" role="tab" aria-controls="v-pills-bafes" aria-selected="false">Βαφές</a>
@@ -20,7 +20,7 @@
                 <a class="nav-link" id="v-pills-provlepsis-tab" data-toggle="pill" href="#v-pills-provlepsis" role="tab" aria-controls="v-pills-provlepsis" aria-selected="false">Προβλέψεις</a>
                 <a class="nav-link" id="v-pills-praktoreia-tab" data-toggle="pill" href="#v-pills-praktoreia" role="tab" aria-controls="v-pills-praktoreia" aria-selected="false">Πρακτορεία</a>
                 <a class="nav-link" id="v-pills-notes-tab" data-toggle="pill" href="#v-pills-notes" role="tab" aria-controls="v-pills-notes" aria-selected="false">Παρατηρήσεις</a>
-                <a class="nav-link" id="v-pills-eksodasynergati-tab" data-toggle="pill" href="#v-pills-eksodasynergati" role="tab" aria-controls="v-pills-eksodasynergati" aria-selected="false">Έξοδα συνεργάτη</a>
+                <a class="nav-link" id="v-pills-eksodasynergati-tab" data-toggle="pill" href="#v-pills-eksodasynergati" role="tab" aria-controls="v-pills-eksodasynergati" aria-selected="false">Έξοδα συνεργάτη</a>--}}
             </div>
         </div>
         <div class="col-10">
@@ -28,26 +28,76 @@
                 <div class="tab-pane fade show active" id="v-pills-genika" role="tabpanel" aria-labelledby="v-pills-genika-tab" style="border: 1px solid black;">
                     <form method="post" action="{{ route('pragmatognomosines.store') }}" autocomplete="off">
                         @csrf
-                        <h6 class="heading-small text-muted mb-4">{{ __('Γενικά / Οικονομικά / Παθών - Υπαίτιος') }}</h6>
-                        <div class="pl-lg-4">
-                            <div class="form-group{{ $errors->has('grafeio') ? ' has-danger' : '' }}">
-                                <label class="form-control-label" for="input-grafeio">{{ __('Grafeio') }}</label>
-                                <input type="text" name="grafeio" id="input-grafeio" class="form-control form-control-alternative{{ $errors->has('grafeio') ? ' is-invalid' : '' }}" placeholder="{{ __('Grafeio') }}" value="{{ old('grafeio') }}" required autofocus>
-
-                                @if ($errors->has('grafeio'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('grafeio') }}</strong>
-                                    </span>
-                                @endif
+                        <div class="ektheseis-header">
+                            <div class="ektheseis-subheader">
+                                <div class="pl-lg-4">
+                                    <h6 class="heading-small text-muted mb-4"><strong>{{ __('Γενικά') }}</strong></h6>
+                                    <div class="form-group{{ $errors->has('Prot_bibliou') ? ' has-danger' : '' }}">
+{{--                                        <label class="form-control-label" for="input-grafeio">{{ __('Grafeio') }}</label>--}}
+                                        <input type="text" name="Prot_bibliou" id="input-Prot_bibliou" class="form-control form-control-alternative{{ $errors->has('Prot_bibliou') ? ' is-invalid' : '' }}" placeholder="{{ __('Αρ. Φακέλου') }}" value="{{ old('Prot_bibliou') }}" required autofocus>
+                                        @if ($errors->has('Prot_bibliou'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('Prot_bibliou') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="pl-lg-4">
+                                    <h6 class="heading-small text-muted mb-4" color="white"><strong >{{ __('Γενικά') }}</strong></h6>
+                                    <div class="form-group{{ $errors->has('date_atiximatos') ? ' has-danger' : '' }}">
+{{--                                        <label class="form-control-label" for="input-date_atiximatos">{{ __('date_atiximatos') }}</label>--}}
+                                        <input type="date" name="date_atiximatos" id="input-date_atiximatos" class="form-control form-control-alternative{{ $errors->has('date_atiximatos') ? ' is-invalid' : '' }}" placeholder="{{ __('Ημ/νια Ατυχήματος') }}" value="{{ old('date_atiximatos') }}" required autofocus>
+                                        @if ($errors->has('date_atiximatos'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('date_atiximatos') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="pl-lg-4">
+                                    <h6 class="heading-small text-muted mb-4" color="white"><strong >{{ __('Γενικά') }}</strong></h6>
+                                    <div class="form-group{{ $errors->has('date_atiximatos') ? ' has-danger' : '' }}">
+                                        <label class="form-control-label" for="input-grafeio">{{ __('Grafeio') }}</label>
+                                        <input type="text" name="grafeio" id="input-grafeio" class="form-control form-control-alternative{{ $errors->has('grafeio') ? ' is-invalid' : '' }}" placeholder="{{ __('Grafeio') }}" value="{{ old('grafeio') }}" required autofocus>
+                                        @if ($errors->has('grafeio'))
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $errors->first('grafeio') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                </div>
                             </div>
-
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-success mt-4">{{ __('Save') }}</button>
+                            <div  class="pl-lg-4" >
+                                <h6 class="heading-small text-muted mb-4"><strong>{{ __('Οικονομικά') }}</strong></h6>
+                                <div class="form-group{{ $errors->has('grafeio') ? ' has-danger' : '' }}">
+                                    <label class="form-control-label" for="input-grafeio">{{ __('Grafeio') }}</label>
+                                    <input type="text" name="grafeio" id="input-grafeio" class="form-control form-control-alternative{{ $errors->has('grafeio') ? ' is-invalid' : '' }}" placeholder="{{ __('Grafeio') }}" value="{{ old('grafeio') }}" required autofocus>
+                                    @if ($errors->has('grafeio'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('grafeio') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
                             </div>
+                            <div  class="pl-lg-4">
+                                <h6 class="heading-small text-muted mb-4"><strong>{{ __('Παθών - Υπαίτιος') }}</strong></h6>
+                                <div class="form-group{{ $errors->has('grafeio') ? ' has-danger' : '' }}">
+                                    <label class="form-control-label" for="input-grafeio">{{ __('Grafeio') }}</label>
+                                    <input type="text" name="grafeio" id="input-grafeio" class="form-control form-control-alternative{{ $errors->has('grafeio') ? ' is-invalid' : '' }}" placeholder="{{ __('Grafeio') }}" value="{{ old('grafeio') }}" required autofocus>
+                                    @if ($errors->has('grafeio'))
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('grafeio') }}</strong>
+                                        </span>
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-danger mt-4" style="margin: 4px">{{ __('Καταχώρηση') }} </button>
                         </div>
                     </form>
                 </div>
-                <div class="tab-pane fade" id="v-pills-antikatastasis" role="tabpanel" aria-labelledby="v-pills-antikatastasis-tab">...</div>
+{{--                <div class="tab-pane fade" id="v-pills-antikatastasis" role="tabpanel" aria-labelledby="v-pills-antikatastasis-tab">...</div>
                 <div class="tab-pane fade" id="v-pills-epanafores" role="tabpanel" aria-labelledby="v-pills-epanafores-tab">...</div>
                 <div class="tab-pane fade" id="v-pills-eksagogi" role="tabpanel" aria-labelledby="v-pills-eksagogi-tab">...</div>
                 <div class="tab-pane fade" id="v-pills-bafes" role="tabpanel" aria-labelledby="v-pills-bafes-tab">...</div>
@@ -61,7 +111,7 @@
                 <div class="tab-pane fade" id="v-pills-provlepsis" role="tabpanel" aria-labelledby="v-pills-provlepsis-tab">...</div>
                 <div class="tab-pane fade" id="v-pills-praktoreia" role="tabpanel" aria-labelledby="v-pills-praktoreia-tab">...</div>
                 <div class="tab-pane fade" id="v-pills-notes" role="tabpanel" aria-labelledby="v-pills-notes-tab">...</div>
-                <div class="tab-pane fade" id="v-pills-eksodasynergati" role="tabpanel" aria-labelledby="v-pills-eksodasynergati-tab">...</div>
+                <div class="tab-pane fade" id="v-pills-eksodasynergati" role="tabpanel" aria-labelledby="v-pills-eksodasynergati-tab">...</div>--}}
             </div>
         </div>
     </div>
