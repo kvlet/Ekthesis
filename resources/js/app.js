8cc880afe86f9cde85c22cb2323e2adf04889992
Vasilis <vasilhs_xd@hotmail.com>
2020-01-06 20:49:46 +0200

    Floating input labels

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,30 @@
  */
 
 require('./bootstrap');
+
+$(document).ready(function(){
+
+    let inputFields = $('.form-group input');
+
+    inputFields.each(function (index, element){
+        if($(element).val().length > 0) {
+            $(element).parent().addClass('active')
+        }
+    });
+
+    inputFields.on('focus', function(e) {
+        e.preventDefault();
+        $(this).parent().addClass('active')
+    });
+
+    inputFields.on('blur', function(e) {
+        e.preventDefault();
+        if($(this).val().length === 0) {
+            $(this).parent().removeClass('active')
+        }
+    });
+
+});
+
+
+
