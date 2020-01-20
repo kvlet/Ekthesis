135bc70f7a0655f5c8e811176866a3f9f2a4f0f7
kvlet <vletsiskostas@gmail.com>
2020-01-20 14:05:41 +0200

    1) update PragmatognomosinesController.php

--- app/Http/Controllers/PragmatognomosinesController.php
+++ app/Http/Controllers/PragmatognomosinesController.php
@@ -67,6 +67,38 @@ class PragmatognomosinesController extends Controller
         $pragmatognomosini->Date_atiximatos = $request->Date_atiximatos;
         $pragmatognomosini->Date_dikasimou = $request->Date_dikasimou;
         $pragmatognomosini->Date_eksetasis = $request->Date_eksetasis;
+        $pragmatognomosini->id_accedent_place = $request->id_accedent_place;
+        $pragmatognomosini->id_ypaitiou = $request->id_ypaitiou;
+        $pragmatognomosini->id_oximatos_ypaitiou = $request->id_oximatos_ypaitiou;
+        $pragmatognomosini->id_pathon = $request->id_pathon;
+        $pragmatognomosini->id_oximatos_pathon = $request->id_oximatos_pathon;
+        $pragmatognomosini->Xiliometra = $request->Xiliometra;
+        $pragmatognomosini->id_company_ypaitiou = $request->id_company_ypaitiou;
+        $pragmatognomosini->id_company_pathon = $request->id_company_pathon;
+        $pragmatognomosini->id_company_pathon_real = $request->id_company_pathon_real;
+        $pragmatognomosini->id_grafeio = $request->id_grafeio;
+        $pragmatognomosini->Valid = $request->Valid;
+        $pragmatognomosini->Simpliromatiki = $request->Simpliromatiki;
+        $pragmatognomosini->Entoleas = $request->Entoleas;
+        $pragmatognomosini->Prot_bibliou = $request->Prot_bibliou;
+        $pragmatognomosini->Sap = $request->Sap;
+        $pragmatognomosini->id_diakrisi = $request->id_diakrisi;
+        $pragmatognomosini->Ekpt_parts = $request->Ekpt_parts;
+        $pragmatognomosini->Ekpt_jobs = $request->Ekpt_jobs;
+        $pragmatognomosini->Date_paradosis = $request->Date_paradosis;
+        $pragmatognomosini->Object = $request->Object;
+        $pragmatognomosini->id = $request->id;
+        $pragmatognomosini->id_arxi_ekdosis_eggrafon = $request->id_arxi_ekdosis_eggrafon;
+        $pragmatognomosini->Fpa = $request->Fpa;
+        $pragmatognomosini->partially_lock = $request->partially_lock;
+        $pragmatognomosini->total_lock = $request->total_lock;
+        $pragmatognomosini->Notes = $request->Notes;
+        $pragmatognomosini->id_nomoi = $request->id_nomoi;
+        $pragmatognomosini->Driver_ypaitiou = $request->Driver_ypaitiou;
+        $pragmatognomosini->value_car_pathon = $request->value_car_pathon;
+        $pragmatognomosini->billing_notes = $request->billing_notes;
+        $pragmatognomosini->driver_pathon = $request->driver_pathon;
+
 
         dd($pragmatognomosini);
         $pragmatognomosini->save();
