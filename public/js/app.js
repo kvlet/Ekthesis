b68a745b3869c4d80126baebdbf569f43d38514f
kvlet <vletsiskostas@gmail.com>
2020-02-18 13:59:02 +0200

    Update app.js
    app.js

--- public/js/app.js
+++ public/js/app.js
@@ -37116,8 +37116,8 @@ $(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/vkolorizos/Documents/code/Ekthesis/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/vkolorizos/Documents/code/Ekthesis/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\ekthesis\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\ekthesis\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
