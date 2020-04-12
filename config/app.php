57f704b2aacf129b9837cd948f320b977c03d48e
kvlet <vletsiskostas@gmail.com>
2020-04-12 17:14:05 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -178,8 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\TelescopeServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Unisharp\Laravelfilemanager\LaravelFilemanagerServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
+//        Unisharp\Laravelfilemanager\LaravelFilemanagerServiceProvider::class,
+//        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
