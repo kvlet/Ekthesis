656d6c92e3a6bae974907249dd5dc178734b559e
kvlet <vletsiskostas@gmail.com>
2020-04-21 02:18:33 +0300

    date picker

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,7 +21,8 @@
 
 {{--    datepicker--}}
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker.min.css" rel="stylesheet"/>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
+{{--    <script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>--}}
+    <script src="{{ URL::asset('js/bootstrap-datepicker.js')}}"></script>
 {{--    datepicker--}}
 
 {{--    editor--}}
