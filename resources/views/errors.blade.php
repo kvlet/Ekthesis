d546c954f3ae783c86d7e50e78e19c426cb89d4d
Konstantinos Vletsis <vletsiskostas@gmail.com>
2020-12-20 16:47:34 +0200

    create Owners

--- resources/views/errors.blade.php
+++ resources/views/errors.blade.php
@@ -2,7 +2,7 @@
     <div class="alert alert-danger" role="alert">
         <ul>
             @foreach ($errors->all() as $error)
-                <li>{{$error}}</li>
+                <li>{{ $error}}</li>
             @endforeach
         </ul>
     </div>
