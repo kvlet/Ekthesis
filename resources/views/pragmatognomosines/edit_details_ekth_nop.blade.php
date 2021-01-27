40a0291a1011e95efb06e5371053d009d5fa8201
Konstantinos Vletsis <vletsiskostas@gmail.com>
2021-01-27 13:41:53 +0200

    detail ekthesis

--- resources/views/pragmatognomosines/edit_details_ekth_nop.blade.php
+++ resources/views/pragmatognomosines/edit_details_ekth_nop.blade.php
@@ -42,7 +42,7 @@
                                                 <label class="form-control-label" for="Cost_job">{{ __('Κόστος Εργασίας') }}</label>
                                                 <input type="number" name="Cost_job" id="Cost_job"
                                                        class="form-control form-input form-control-alternative{{ $errors->has('Cost_job') ? ' is-invalid' : '' }}"
-                                                       value="{{ $detail->pivot->Cost_job }}"  autofocus>
+                                                       value="{{ $detail->pivot->Cost_job }}"  autofocus required>
                                                 @if ($errors->has('Cost_job'))
                                                     <span class="invalid-feedback" role="alert">
                                                      <strong>{{ $errors->first('Cost_job') }}</strong>
