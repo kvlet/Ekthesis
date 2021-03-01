fbac99e4a72749fefaf86c41f038ec1ad468674d
Vasilis <vasilhs_xd@hotmail.com>
2021-03-01 23:50:34 +0200

    Fix navigation menu and clean up layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -36,7 +36,22 @@ $(document).ready(function() {
     $("#diakr_view").DataTable();
 });
 
+$(document).on('click', '.dropdown-menu', function (e) {
+    e.stopPropagation();
+});
 
+// make it as accordion for smaller screens
+if ($(window).width() < 992) {
+    $('.dropdown-menu a').click(function(e){
+        e.preventDefault();
+        if($(this).next('.submenu').length){
+            $(this).next('.submenu').toggle();
+        }
+        $('.dropdown').on('hide.bs.dropdown', function () {
+            $(this).find('.submenu').hide();
+        })
+    });
+}
 
 
 
--- resources/views/layouts/_partials/header.blade.php
+++ resources/views/layouts/_partials/header.blade.php
@@ -3,22 +3,42 @@
 @endsection
 
 @section('logo')
-    <a href="/" target="">Αρχική</a>
-@endsection
-
-@section('header-left')
     <a href="https://www.ksoftdev.gr" target="_blank"><img src="/images/logo.jpg" width="25" height="25" alt="K Software Development" />K Software Development</a>
 @endsection
 
-@section('header-center')
-    Υποστήριξη: <a href="mailto:support@ksoftdev.gr" alt="send mail to suppport">support@ksoftdev.gr</a>
-@endsection
-
 @section('header-right')
-     Έκδοση: v.1.0
+    @auth
+        <nav class="navbar navbar-expand p-0">
+            <div class="navbar-collapse collapse order-3">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item position-relative">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            {{ auth()->user()->F_name . ' ' .  auth()->user()->L_name }}
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="/settings">Ρυθμίσεις</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="{{ url('/logout') }}"> Αποσύνδεση </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    @endauth
 @endsection
 
 
 @section('footer')
-    © 2019 Copyright: K Software Development
+    <div class="d-flex justify-content-between align-items-center">
+        <div>
+            Έκδοση: v.1.0
+        </div>
+        <div class="text-center">
+            © 2019 Copyright: K Software Development
+        </div>
+        <div>
+            Υποστήριξη: <a href="mailto:support@ksoftdev.gr" alt="send mail to suppport">support@ksoftdev.gr</a>
+        </div>
+    </div>
+
 @endsection
