413027a059c965b06dc71c219ee5f21e86482af2
Konstantinos Vletsis <vletsiskostas@gmail.com>
2021-03-05 13:09:31 +0200

    grid css

--- public/js/app.js
+++ public/js/app.js
@@ -37032,9 +37032,23 @@ $(document).ready(function () {
     }
   });
 });
-$(document).ready(function () {
-  $("#diakr_view").DataTable();
-});
+$(document).on('click', '.dropdown-menu', function (e) {
+  e.stopPropagation();
+}); // make it as accordion for smaller screens
+
+if ($(window).width() < 992) {
+  $('.dropdown-menu a').click(function (e) {
+    e.preventDefault();
+
+    if ($(this).next('.submenu').length) {
+      $(this).next('.submenu').toggle();
+    }
+
+    $('.dropdown').on('hide.bs.dropdown', function () {
+      $(this).find('.submenu').hide();
+    });
+  });
+}
 
 /***/ }),
 
